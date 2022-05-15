e045ddc36ba8ad803d16fad086f01b8b925403d5
efikarl <efikarl@yeah.net>
2022-05-15 23:49:20 +0800

    23.2 refer to "n ball to m basket" question
    Signed-off-by: efikarl <efikarl@yeah.net>

--- 23/main.c
+++ 23/main.c
@@ -8,7 +8,10 @@
     THE PROGRAM IS DISTRIBUTED UNDER THE MIT LICENSE ON AN "AS IS" BASIS,
     WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 ---------------------------------------------------------------------------------
-    h   [i].v                   b i t
+    叠积木 => 将集合划分为和相等的最大数量子集问题
+---------------------------------------------------------------------------------
+    假如： (1, 2, 3) => (1, 2) + (3):
+    n   [i].v                   b i t
     1 = [0].1                   0 0 1
     2 = [1].2                   0 1 0
     3 = [2].3                   1 0 0
@@ -17,8 +20,8 @@
     6 = [1].2, [2].3            1 1 0
     7 = [0].1, [1].2, [2].3     1 1 1
 ---------------------------------------------------------------------------------
-    [i]
-    [o]
+    [i] 空格分隔的积木长度
+    [o] 长度相等的最高高度
 ---------------------------------------------------------------------------------
 --*/
 #include <stdio.h>
@@ -27,8 +30,6 @@
 #include <assert.h>
 #include <string.h>
 
-#include <lszlib.h>
-
 // #define NDEBUG
 
 #ifndef NDEBUG
@@ -36,29 +37,15 @@
 #endif
 
 #define MAX_SET     5
-
-typedef struct {
-    int             index;
-    int             value;
-} set_ele_t;
-
-typedef struct {
-    int             cnt;
-    set_ele_t      *ele;
-    int             sum;
-} sub_set_t;
+#define MAX_LEN     5
 
 struct {
     int             set[MAX_SET];
     int             n;
-    int             subset_v[MAX_SET];
-    int             subset_i[MAX_SET];
+    int             sub[MAX_SET];
+    int             has[MAX_SET];
     int             m;
-    int             i;
-    void           *subset_tree;
-    int             subset_base;
-    sub_set_t       subset_data;
-    void           *subcmb_tree;
+    int             r;
 } g_slt;
 
 
@@ -71,74 +58,64 @@ int in_parse()
             break;
         }
     }
-    g_slt.subset_tree = tree_new(sizeof(int), sizeof(sub_set_t), lsz_icmp);
+
     return 0;
 }
 
-void build_subset()
+// 将N元素装M桶问题
+void subs(int next, int i)
 {
-    g_slt.subset_base++;
-
-    g_slt.subset_data.cnt = g_slt.m;
-    g_slt.subset_data.ele = calloc(g_slt.m, sizeof(set_ele_t));
-    g_slt.subset_data.sum = 0;
-    for (int i = 0; i < g_slt.m; i++) {
-        g_slt.subset_data.ele[i].index = g_slt.subset_i[i];
-        g_slt.subset_data.ele[i].value = g_slt.subset_v[i];
-        g_slt.subset_data.sum         += g_slt.subset_v[i];
-    }
-
-    tree_insert(g_slt.subset_tree, &g_slt.subset_base, &g_slt.subset_data);
-}
-
-void subs(int n)
-{
-    if (g_slt.i + g_slt.n - n < g_slt.m) {
+    if (next >= g_slt.n) {
+        // 所有球都放完了，看看是否每个桶都相等
+        i = 1;
+        while ((i < g_slt.m) && (g_slt.sub[i-1] == g_slt.sub[i])) {
+            if (!g_slt.has[i-1] || !g_slt.has[i]) {
+                break;
+            } else {
+                i++;
+            }
+        }
+        if (i >= g_slt.m) {
+            if (g_slt.sub[0] <= MAX_LEN) {
+                g_slt.r = g_slt.m;
+            }
+        }
         return;
     }
-    if (g_slt.i >= g_slt.m) {
-        build_subset();
-        return;
-    } else {
-        g_slt.subset_i[g_slt.i] = n;
-        g_slt.subset_v[g_slt.i] = g_slt.set[n];
-        g_slt.i++;
-        subs(n + 1);
-        g_slt.i--;
-        subs(n + 1);
+
+    // 每个元素都有M种选择
+    for (i = 0; i < g_slt.m; i++) {
+
+        // 对每个桶：
+
+        // （1）就是它了~
+        g_slt.sub[i] += g_slt.set[next];
+        g_slt.has[i] ++;
+        subs(next + 1, i);
+        // （2）再考虑考虑
+        g_slt.sub[i] -= g_slt.set[next];
+        g_slt.has[i] --;
     }
-}
-
-void tree_for_each_callback(lsz_tree_t *node, void *data)
-{
-    sub_set_t *subset = node->data;
-
-#ifndef NDEBUG
-    for (int i = 0; i < subset->cnt; i++) {
-        if (i == 0) {
-            printf("(");
-        }
-
-        printf("[%d].%d", subset->ele[i].index, subset->ele[i].value);
-
-        if (i != subset->cnt - 1) {
-            printf(",");
-        } else {
-            printf(")\n");
-        }
-    }
-#endif
 }
 
 int solution()
 {
     // code
-    for (int i = 1; i < g_slt.n; i++) {
+    for (int i = g_slt.n; i > 0; i--) {
         g_slt.m = i;
-        g_slt.i = 0;
-        subs(0);
+        g_slt.r = 0;
+        memset(g_slt.sub, 0, g_slt.m);
+        memset(g_slt.has, 0, g_slt.m);
+
+        subs(0, 0);
+        if (g_slt.r) {
+            break;
+        }
     }
-    tree_for_each(g_slt.subset_tree, tree_for_each_callback, NULL);
+    if (!g_slt.r) {
+        g_slt.r = -1;
+    }
+    printf("%d\n", g_slt.r);
 
     return 0;
 }
